7fe2984e5173f160c6fb0a6a3459e9f52fe977ac
Goran <gospaso@gmail.com>
2017-03-13 18:02:05 -0400

    update tests, add scripts
    - 100% coverage

--- src/lexrank.spec.js
+++ src/lexrank.spec.js
@@ -1,25 +1,24 @@
 const chai = require('chai')
+const fs = require('fs')
 const lexrank = require('./lexrank')
 
 const expect = chai.expect
+const text = fs.readFileSync('./fixtures/test.txt', 'utf8')
 
 describe('lexrank.js', () => {
-  const text = `Automatic summarization is the process of reducing a text document with a computer program in order to create a summary that retains the most important points of the original document. Technologies that can make a coherent summary take into account variables such as length, writing style and syntax. Automatic data summarization is part of machine learning and data mining. The main idea of summarization is to find a representative subset of the data, which contains the information of the entire set. Summarization technologies are used in a large number of sectors in industry today. An example of the use of summarization technology is search engines such as Google. Other examples include document summarization, image collection summarization and video summarization. Document summarization, tries to automatically create a representative summary or abstract of the entire document, by finding the most informative sentences. Similarly, in image summarization the system finds the most representative and important (or salient) images. Similarly, in consumer videos one would want to remove the boring or repetitive scenes, and extract out a much shorter and concise version of the video. This is also important, say for surveillance videos, where one might want to extract only important events in the recorded video, since most part of the video may be uninteresting with nothing going on. As the problem of information overload grows, and as the amount of data increases, the interest in automatic summarization is also increasing.\n\nGenerally, there are two approaches to automatic summarization: extraction and abstraction. Extractive methods work by selecting a subset of existing words, phrases, or sentences in the original text to form the summary. In contrast, abstractive methods build an internal semantic representation and then use natural language generation techniques to create a summary that is closer to what a human might generate. Such a summary might contain words not explicitly present in the original. Research into abstractive methods is an increasingly important and active research area, however due to complexity constraints, research to date has focused primarily on extractive methods. In some application domains, extractive summarization makes more sense. Examples of these include image collection summarization and video summarization.`
-  const expectedSummary = `Automatic summarization is the process of reducing a text document with a computer program in order to create a summary that retains the most important points of the original document. The main idea of summarization is to find a representative subset of the data, which contains the information of the entire set. As the problem of information overload grows, and as the amount of data increases, the interest in automatic summarization is also increasing.`
-  const result = lexrank.analyze(text)
   describe('analyze(text)', () => {
-    it('should return ranked sentences', () => {
-      expect(result.ranked).to.be.a('array')
-      expect(result.ranked.length).to.equal(19)
-    })
-    it('should return a summary', () => {
-      expect(result.summary).to.be.a('string')
-      expect(result.summary).to.equal(expectedSummary)
+    const expected = `Automatic summarization is the process of reducing a text document with a computer program in order to create a summary that retains the most important points of the original document.`
+    const result = lexrank.analyze(text)
+    it('should return arrays of ranked sentences', () => {
+      expect(result).to.be.a('array')
+      expect(result[0]).to.be.a('array')
+      expect(result.length).to.equal(8)
+      expect(result[0].length).to.equal(11)
     })
     it('should accept a callback', () => {
-      lexrank.analyze(text, (err, res) => {
-        expect(res.ranked.length).to.equal(19)
-        expect(res.summary).to.equal(expectedSummary)
+      lexrank.analyze(text, (_, res) => {
+        expect(res.length).to.equal(8)
+        expect(res[0][0].text).to.equal(expected)
       })
     })
   })
--- src/utils.spec.js
+++ src/utils.spec.js
@@ -1,52 +1,96 @@
 const chai = require('chai')
+const fs = require('fs')
 const utils = require('./utils')
 
 const expect = chai.expect
+const text = fs.readFileSync('./fixtures/test.txt', 'utf8')
 
 describe('utils.js', () => {
+  describe('flatten()', () => {
+    it('should flatten an array of objects', () => {
+      const array = [[1, 2, 3], [4, 5, 6]]
+      expect(utils.flatten(array).length).to.equal(6)
+    })
+  })
+  describe('normalize()', () => {
+    it('should normalize an array of numbers', () => {
+      const array = [0.1, 0.2, 0.5]
+      const expected = [0.2, 0.4, 1]
+      expect(utils.normalize(array)).to.eql(expected)
+    })
+  })
+  describe('tanimoto()', () => {
+    it('should return a small tanimoto distance for two dissimilar sentences', () => {
+      const a = utils.wordsArray('This is one really awesome sentence')
+      const b = utils.wordsArray('This is a much more different sentence')
+      expect(utils.tanimoto(a, b)).to.equal(0.3)
+    })
+    it('should return a large tanimoto distance for two similar sentences', () => {
+      const a = utils.wordsArray('This is definitely a really awesome sentence')
+      const b = utils.wordsArray('This is definitely a really similar sentence')
+      expect(utils.tanimoto(a, b)).to.equal(0.75)
+      expect(utils.tanimoto(a, a)).to.equal(1)
+    })
+  })
+  describe('eigenvalues()', () => {
+    it('should scale and normalize matrix scores', () => {
+      const sentences = utils.sentencesArray(utils.paragraphsArray(text)[0])
+      const matrix = utils.wordsMatrix(sentences.map(utils.wordsArray))
+      const eigen = utils.eigenvalues(matrix, sentences)
+      expect(sentences.length).to.equal(eigen.length)
+      expect(sentences[0]).to.be.a('string')
+      expect(eigen[0]).to.be.a('number')
+    })
+  })
+  describe('paragraphsArray()', () => {
+    it('should return an array of paragraphs from text', () => {
+      const result = utils.paragraphsArray(text)
+      expect(result.length).to.equal(8)
+    })
+  })
   describe('sentencesArray()', () => {
     it('should return an array of sentences from text', () => {
-      const text = `This is one sentence. "This is quoted text" in a sentence.\n\n` +
-        `This sentence is on a new line! This sentence: is split -- up.`
       const result = utils.sentencesArray(text)
-      expect(result.length).to.equal(4)
+      expect(result.length).to.equal(24)
     })
   })
   describe('wordsArray()', () => {
     it('should return an array of words from text', () => {
-      const text = `This sentence: is split -- up.`
-      const expected = ['this', 'sentence:', 'is', 'split', '--', 'up.']
-      const result = utils.wordsArray(text)
-      expect(result).to.deep.equal(expected)
+      const expected = utils.wordsArray('Automatic summarization is the process of reducing a text document with a computer program in order to create a summary that retains the most important points of the original document.')
+      const result = utils.wordsArray(utils.sentencesArray(utils.paragraphsArray(text)[0])[0])
+      expect(result).to.eql(expected)
     })
   })
-  describe('constructMatrix()', () => {
+  describe('wordsMatrix()', () => {
     it('should construct a two-dimentional matrix of tanimoto distance scores', () => {
-      const text = `This is one sentence. "This is quoted text" in a sentence.\n\n` +
-        `This sentence is on a new line! This sentence: is split -- up.`
+      const text = `This is one sentence. 'This is quoted text' in a sentence.\n\n` +
+        `This sentence is in a new paragraph! This sentence: is split right up.`
       const sentencesAndWords = utils.sentencesArray(text).map(utils.wordsArray)
-      const matrix = utils.constructMatrix(sentencesAndWords)
-      const expected = [0.9121556554139507, 0.2736466966241852, 0.20270125675865572, 0.22803891385348768]
+      const matrix = utils.wordsMatrix(sentencesAndWords)
+      const expected = [1, 0.2222222222222222, 0.2222222222222222, 0.25]
       expect(matrix.length).to.equal(4)
-      expect(matrix[0]).to.deep.equal(expected)
+      expect(matrix[0]).to.eql(expected)
+    })
+    it('should not rank sentences below a provided threshold', () => {
+      const text = `This is one sentence. 'This is quoted text' in a sentence.\n\n` +
+        `This sentence is in a new paragraph! This sentence: is split right up.`
+      const sentencesAndWords = utils.sentencesArray(text).map(utils.wordsArray)
+      const matrix = utils.wordsMatrix(sentencesAndWords, 0.23)
+      const expected = [1, 0, 0, 0.25]
+      expect(matrix.length).to.equal(4)
+      expect(matrix[0]).to.eql(expected)
     })
   })
   describe('pageRank()', () => {
     it('should return objects with scores for each sentence', () => {
-      const text = `This is one sentence. "This is quoted text" in a sentence.\n\n` +
-        `This sentence is on a new line! This sentence: is split -- up.`
-      const sentencesRaw = utils.sentencesArray(text)
-      const sentencesOriginal = [...sentencesRaw]
-      const sentencesAndWords = sentencesRaw.map(utils.wordsArray)
-      const matrix = utils.constructMatrix(sentencesAndWords)
-      const sentencesRanked = utils.pageRank(matrix, sentencesOriginal)
-      const expected = {
-        weight: 0.5447947944386553,
-        text: '"This is quoted text" in a sentence.',
-        index: 1
+      const sentences = utils.sentencesArray(text)
+      const ranked = utils.pageRank(sentences)
+      const expected = { weight: 1,
+        text: 'Automatic summarization is the process of reducing a text document with a computer program in order to create a summary that retains the most important points of the original document.',
+        index: 0
       }
-      expect(sentencesRanked.length).to.equal(4)
-      expect(sentencesRanked).to.contain(expected)
+      expect(ranked.length).to.equal(24)
+      expect(ranked).to.contain(expected)
     })
   })
 })
